db53abb8967c308f887fba5fc5de6e69a31197c9
DevJake99 <jacobg4@uci.edu>
2023-12-05 17:49:38 -0800

    moved all files up one lvl in directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact'
+import Contact from './components/Contact/Contact.jsx'
 import ProjectPage from './components/Projects/ProjectPage.jsx'
 
 function App() {
