7f389c02ce852022b0a45122130c17805934d2fa
Jacob Garcia <jacobgarcia@Jacobs-MacBook-Pro.local>
2024-02-11 20:15:40 -0800

    Added Weather App Project to project list with descriptions and links

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -3,6 +3,7 @@ import MVCTechBlog from '../assets/images/mvctechblog.png'
 import MealShare from '../assets/images/MealShare.png'
 import ReadMEGen from '../assets/images/ReadmeGen.png'
 import BookRefactor from '../assets/images/BookRefactor.png'
+import WeatherApp from '../assets/images/WeatherApp.png'
 
 
 export const projectList = [
@@ -48,6 +49,15 @@ export const projectList = [
         MongoDB Database. Users can create accounts ( which are later verified using JWT) to save their favorite book and create a reading list for later. `,
         link: 'https://booksearchrefactor-9e105cf8023f.herokuapp.com/',
         github: 'https://github.com/DevJake99/Book-Search-Refactor'
+    },
+    {
+        name: 'Weather App',
+        image: WeatherApp,
+        description: `This application uses the Open Weather Map API to fetch the current weather, as well as a 5-day forecast for any city. This site is a simple 
+        page I created using HTML, JavaScript and CSS. Some skills I learned from working on this project include working with the DOM, using and implementing local storage
+        and navigating/fetching dynamic data with API's `,
+        link: 'https://devjake99.github.io/WeatherApp/',
+        github: 'https://github.com/DevJake99/WeatherApp'
     },
 
 ]
